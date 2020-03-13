c5a3aad05e5ba53e83c81ff8760cbb0e4a392cdb
LeeJiSun <j5119s@naver.com>
2020-03-13 19:26:35 +0900

    check

--- Programmers/Sort/Biggest_num.java
+++ Programmers/Sort/Biggest_num.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 
 class Biggest_num {
     public static String solution(int[] numbers) {
@@ -9,17 +10,23 @@ class Biggest_num {
         for(int i = 0 ; i < num.length ; i++)
             num[i] = Integer.toString(numbers[i]);
         
-        Arrays.sort(num);
+        Arrays.sort(num, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2.concat(o1)).compareTo(o1.concat(o2));
+            }
+        });
+
+        return String.join("", num);
         
+        // for(String s : num)
+        //     System.out.print(s + " ");
 
-        for(String s : num)
-            System.out.print(s.valueOf + " ");
-
-        return answer;
+        // return answer;
     }
 
     public static void main(String[] args) {
         int[] numbers = {3, 30, 34, 5, 9};
-        solution(numbers);
+        System.out.println(solution(numbers));
     }
 }
