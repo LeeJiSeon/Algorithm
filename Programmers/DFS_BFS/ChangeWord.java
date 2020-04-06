b3bd0b1112e9b6e4491c6e1b51da9efdd2471b17
LeeJiSun <j5119s@naver.com>
2020-04-06 18:32:44 +0900

    단어 변환

--- Programmers/DFS_BFS/ChangeWord.java
+++ Programmers/DFS_BFS/ChangeWord.java
@@ -1,15 +1,37 @@
+import java.util.*;
 class ChangeWord {
+    boolean[] visited;
+    String target = "";
+    int min = 100;
+
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        for(int i = 0; i < words.length; i++) {
-            boolean visited = new boolean[words.length];
-            dfs(begin, words);
+        visited = new boolean[words.length];
+        this.target = target;
+        dfs(begin, words, 0);
+        return min == 100 ? 0 : min;
+    }
+
+    private void dfs(String begin, String[] words, int count) {
+        for(int i = 0 ; i < words.length ; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                if(checkWord(begin, words[i])) {
+                    if(words[i].equals(target))
+                        min = Math.min(min, count + 1);
+                    else
+                        dfs(words[i], words, count + 1);
+                }
+                visited[i] = false;
+            }
         }
-        return answer;
     }
 
-    static void dfs(String begin, String[] words) {
-        for(int i = 0 ; i < words.length ; i++)
+    private boolean checkWord(String begin, String target) {
+        int count = 0;
+        for(int i = 0; i < begin.length() ; i++)
+            if(begin.charAt(i) != target.charAt(i))
+                count++;
+        if(count == 1)  return true;
+        return false;
     }
-
 }
