f0a6a236d7de6567fb835e7742473acf86ed97ee
LeeJiSun <j5119s@naver.com>
2020-05-06 14:56:21 +0900

    소수 찾기

---
+++ BOJ/Math/Sol1978.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Sol1978 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for(int i = 0 ; i < n ; i++) {
+            int num = sc.nextInt();
+            if(num == 1)
+                continue;
+            boolean isPrime = true;
+            for(int j = 2 ; j <= Math.sqrt(num) ; j++)
+                if(num % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            if(isPrime)
+                count++;
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
