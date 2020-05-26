e09e590e7ae0875b6c815a90e849efd98562ff29
LeeJiSun <j5119s@naver.com>
2020-05-26 11:05:41 +0900

    숫자의 표현

---
+++ Programmers/Practice/NumExpression.java
@@ -0,0 +1,14 @@
+class NumExpression {
+    public int solution(int n) {
+        int answer = 1;
+        for(int i = 1 ; i <= n/2 ; i++) {
+            int sum = 0;
+            int j = i;
+            while(sum < n)
+                sum += j++;
+            if(sum == n)
+                answer++;
+        }
+        return answer;
+    }
+}
