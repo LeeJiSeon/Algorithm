d1d3a5cdb411ae8e5bf61c06e0ca6be78e772f92
LeeJiSun <j5119s@naver.com>
2020-06-08 14:50:29 +0900

    소수 만들기

---
+++ Programmers/Summer_Winter/MakePrime.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+class MakePrime {
+    public int solution(int[] nums) {
+        return combination(nums, 0, 0, nums.length, 3, 0);
+    }
+
+    public int combination(int[] nums, int sum, int start, int n, int r, int answer) {
+        if(r == 0) {
+            answer += isPrime(sum) ? 1 : 0;
+            return answer;
+        }
+
+        for(int i = start ; i < n ; i++) {
+            sum += nums[i];
+            answer = combination(nums, sum, i+1, n, r-1, answer);
+            sum -= nums[i];
+        }
+
+        return answer;
+    }
+
+    public boolean isPrime(int sum) {
+        for(int i = 2 ; i <= Math.sqrt(sum) ; i++)
+            if(sum % i == 0)
+                return false;
+        return true;
+    }
+}
