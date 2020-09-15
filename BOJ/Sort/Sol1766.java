3462712cbc54349f01ea552ee3fd75c2268d860d
JISEON <j5119s@naver.com>
2020-09-15 11:30:24 +0900

    문제집 (위상정렬)

---
+++ BOJ/Sort/Sol1766.java
@@ -0,0 +1,35 @@
+//Topology Sort
+import java.util.*;
+class Sol1766 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] indegree = new int[n + 1];
+        List<Integer>[] list = new ArrayList[n + 1];
+        for(int i = 1 ; i <= n ; i++)
+            list[i] = new ArrayList<>();
+        for(int i = 0 ; i < m ; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            list[x].add(y);
+            indegree[y]++;
+        }
+        sc.close();
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int i = 1 ; i <= n ; i++)
+            if(indegree[i] == 0)
+                queue.offer(i);
+        while(!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.print(current + " ");
+            for(int next : list[current]) {
+                indegree[next]--;
+                if(indegree[next] == 0)
+                    queue.offer(next);
+            }
+        }
+        
+    }
+}
