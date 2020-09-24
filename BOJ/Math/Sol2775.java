abdb95d4ffeea354aca1ab9978c254af5e0907c3
JISEON <j5119s@naver.com>
2020-09-24 11:40:24 +0900

    부녀회장이 될테야

---
+++ BOJ/Math/Sol2775.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Sol2775 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for(int cnt = 0 ; cnt < t ; cnt++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            int[][] dp = new int[k+1][n+1];
+            for(int i = 1 ; i <= n ; i++)
+                dp[0][i] = i;
+            for(int i = 1 ; i <= k ; i++)
+                for(int j = 1 ; j <= n ; j++)
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            System.out.println(dp[k][n]);
+        }
+    }
+}
