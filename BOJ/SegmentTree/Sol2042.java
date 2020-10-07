d150c5d01f9185579a5f938262514dd2ea551112
JISEON <j5119s@naver.com>
2020-10-07 15:16:41 +0900

    구간 합 구하기

--- BOJ/SegmentTree/Sol2042.java
+++ BOJ/SegmentTree/Sol2042.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
+class Sol2042 {
     static long[] arr, tree;
     
     public static void main(String[] args) throws IOException {
