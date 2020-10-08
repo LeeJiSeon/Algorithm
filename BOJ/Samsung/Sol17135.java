91efad3b431924097523925c7e6d7cb624aa3def
JISEON <j5119s@naver.com>
2020-10-08 15:36:42 +0900

    캐슬 디펜스

--- BOJ/Samsung/Sol17135.java
+++ BOJ/Samsung/Sol17135.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.util.*;
 
 class Enemy implements Comparable<Enemy> {
