fe204b67200f1a7c4717730449452edd016f2a46
JISEON <j5119s@naver.com>
2020-10-16 17:15:19 +0900

    최대공약수와 최소공배수 (유클리드 호제법)

---
+++ BOJ/GCD_LCM/Sol2609.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Sol2609 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        if(a < b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        
+        int gcd = Euclidean(a, b);
+        int lcm = a * b / gcd;
+        
+        System.out.println(gcd);
+        System.out.println(lcm); 
+        
+        br.close();
+    }
+    
+    private static int Euclidean(int a, int b) {
+        if(b == 0)  return a;
+        return Euclidean(b, a % b);
+    }
+
+}
