816523bec607ee3de0f8d70e7e9922101e868241
JISEON <j5119s@naver.com>
2020-10-21 11:42:34 +0900

    토마토

--- BOJ/DFS_BFS/Sol7569.java
+++ BOJ/DFS_BFS/Sol7569.java
@@ -8,20 +8,18 @@ import java.util.LinkedList;
 public class Sol7569 {
     static int n, m, h;
     static int[][][] box;
-    static boolean[][][] visited;
     static int[] dy = {-1, 1, 0, 0};
     static int[] dz = {0, 0, -1, 1};
     
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		m = Integer.parseInt(st.nextToken());
-		n = Integer.parseInt(st.nextToken());
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    	
+	    m = Integer.parseInt(st.nextToken());
+	    n = Integer.parseInt(st.nextToken());
 	    h = Integer.parseInt(st.nextToken());
 	    
 	    box = new int[h][n][m];
-	    visited = new boolean[h][n][m];
 	    
 	    for(int i = 0; i < h; i++) {
 	        for(int j = 0; j < n; j++) {
@@ -44,10 +42,8 @@ public class Sol7569 {
 	    for(int i = 0; i < h; i++)
 	        for(int j = 0; j < n; j++)
 	            for(int k = 0; k < m; k++)
-	                if(box[i][j][k] == 1) {
+	                if(box[i][j][k] == 1)
 	                    queue.offer(new Node(i, j, k));
-	                    visited[i][j][k] = true;
-	                }
 
 	    while(!queue.isEmpty()) {
 	        Node node = queue.poll();
@@ -57,26 +53,23 @@ public class Sol7569 {
 	            int ny = y + dy[d];
 	            int nz = z + dz[d];
 	            
-	            if(ny < 0 || ny >= n || nz < 0 || nz >= m || visited[x][ny][nz] || box[x][ny][nz] == -1)
+	            if(ny < 0 || ny >= n || nz < 0 || nz >= m || box[x][ny][nz] != 0)
 	                continue;
 	                
 	            box[x][ny][nz] = box[x][y][z] + 1;
 	            cnt = box[x][y][z];
 	            queue.offer(new Node(x, ny, nz));
-	            visited[x][ny][nz] = true;
 	        }
 	        
 	        if(x - 1 >= 0 && x - 1 < h && box[x-1][y][z] == 0) {
 	            box[x-1][y][z] = box[x][y][z] + 1;
 	            cnt = box[x][y][z];
 	            queue.offer(new Node(x-1, y, z));
-	            visited[x-1][y][z] = true;
 	        }
 	        if(x + 1 >= 0 && x + 1 < h && box[x+1][y][z] == 0) {
 	            box[x+1][y][z] = box[x][y][z] + 1;
 	            cnt = box[x][y][z];
 	            queue.offer(new Node(x+1, y, z));
-	            visited[x+1][y][z] = true;
 	        }
 	    }
 	    
